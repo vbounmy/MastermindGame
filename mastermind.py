7e84f27e0d6cc9be0a5b2c9c9dcea557488f9a48
vbounmy <valentine.bounmy@gmail.com>
2024-12-19 23:16:40 +0100

    Adding ansi_codes

--- mastermind.py
+++ mastermind.py
@@ -1,26 +1,14 @@
 from random import choice
+from ansi_escape_codes import *
 
 # possible_colors = ["🔴", "🟠", "🟡", "🟢", "🔵", "🟣"]
 # possible_colors = ["\U+1F534", "\U+1F7E0", "\U+1F7E1", "\U+1F7E2", "\U+1F535", "\U+1F7E3"]
-
-class Style:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ITALIC = '\x1B[3m'
-    END = '\033[0m'
 
 def create_combination(possible_numbers, combination_size):
     combination = []
     for _ in combination_size:
         combination += choice(possible_numbers)
-    print("secret combination :" + str(combination))
+    # print("secret combination :" + str(combination))
     return combination
 
 def player_gameplay(possible_numbers, combination_size):
@@ -38,7 +26,7 @@ def player_gameplay(possible_numbers, combination_size):
             print("You entered an invalid number. Please choose among the possible numbers.")
         else:
             valid_proposition = True
-            print("\n" + 'Your proposition : ' + "\t" + '\033[1m' + str(player_proposition_in_list) + '\033[0m')
+            print("\n" + 'Your proposition : ' + "\t" + BOLD + str(player_proposition_in_list) + END)
 
     return player_proposition_in_list
 
@@ -48,7 +36,7 @@ def verification(player_proposition_in_list, combination):
 
     for index, number in enumerate(player_proposition_in_list):
         if number == temp_combination[index]:
-            hints.append('\033[1m' + '\033[92m' + "o" + '\033[0m')
+            hints.append(BOLD + GREEN + "o" + END)
             temp_combination[index] = None
         else:
             hints.append(None)
@@ -56,10 +44,10 @@ def verification(player_proposition_in_list, combination):
     for index, number in enumerate(player_proposition_in_list):
         if hints[index] == None:
             if number in temp_combination:
-                hints[index] = '\033[1m' + '\033[93m' + "-" + '\033[0m'
+                hints[index] = BOLD + YELLOW + "-" + END
                 temp_combination[temp_combination.index(number)] = None
             else:
-                hints[index] = '\033[1m' + '\033[91m' + "x" + '\033[0m'
+                hints[index] = BOLD + RED + "x" + END
 
     print("Hints : " + "\t"*2 + "[" + ', '.join(hints) + "]")
 
@@ -72,25 +60,25 @@ def create_game():
 
     combination = create_combination(possible_numbers, combination_size)
 
-    print('\033[1m' + '\033[94m' + "\n" + "MASTERMIND GAME" + '\033[0m')
-    print('\x1B[3m' + "Try to find the secret combination in 10 attempts." + "\n"*2 + "Hints will be given at the end of each round :" + "\n" + "x : incorrect answer, - : right number but wrong position, o : right answer" +'\033[0m' + "\n")
+    print(BOLD + BLUE + "\n" + "MASTERMIND GAME" + END)
+    print(ITALIC + "Try to find the secret combination in 10 attempts." + "\n"*2 + "Hints will be given at the end of each round :" + "\n" + "x : incorrect answer, - : right number but wrong position, o : right answer" + END + "\n")
 
     for _ in max_number_of_attempts:
         number_of_attempts += 1
         print("-"*45)
-        print('\033[1m' + "Number of attempts : " + str(number_of_attempts) + "/10" + '\033[0m')
+        print(BOLD + "Number of attempts : " + str(number_of_attempts) + "/10" + END)
         player_proposition_in_list = player_gameplay(possible_numbers, combination_size)
         hints = verification(player_proposition_in_list, combination)
 
-        if hints == ['\033[1m' + '\033[92m' + "o" + '\033[0m', '\033[1m' + '\033[92m' + "o" + '\033[0m', '\033[1m' + '\033[92m' + "o" + '\033[0m', '\033[1m' + '\033[92m' + "o" + '\033[0m']:
+        if hints == [BOLD + GREEN + "o" + END, BOLD + GREEN + "o" + END, BOLD + GREEN + "o" + END, BOLD + GREEN + "o" + END]:
             game_won = True
-            print('\033[1m' + '\033[92m' + "\n" + "Victory." + "\n" + "You found the secret combination, GG !" + '\033[0m')
+            print(BOLD + GREEN + "\n" + "Victory." + "\n" + "You found the secret combination, GG !" + END)
             break
         elif game_won == False:
             if number_of_attempts < 10:
                 print("Wrong combination, please retry." + "\n")
             else:
-                print('\033[1m' + '\033[91m' + "\n" + "Defeat." + "\n" + "The secret combination was " + str(combination) + '\033[0m')
+                print(BOLD + RED + "\n" + "Defeat." + "\n" + "The secret combination was " + str(combination) + END)
     
 
 combination_size = ["", "", "", ""]
